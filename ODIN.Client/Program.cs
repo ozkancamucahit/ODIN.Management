60d1170790ad82c357b2a117afc7e90eed3a5f90
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-02-09 12:29:24 +0300

    added client scripts and auth message dialogs

--- ODIN.Client/Program.cs
+++ ODIN.Client/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ODIN.Client;
+using Syncfusion.Blazor;
+using Syncfusion.Blazor.Popups;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -24,6 +26,8 @@ builder.Services
     .AddScoped<GetHttpClient>()
     .AddScoped<LocalStorageService>()
     .AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>()
-    .AddScoped<IUserAccount, UserAccountService>();
+    .AddScoped<IUserAccount, UserAccountService>()
+    .AddScoped<SfDialogService>()
+    .AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
